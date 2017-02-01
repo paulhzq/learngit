2767b517e6ce903c960444b460f99d5c2b80fdd3
Paul Huang <paul_hzq@hotmail.com>
2017-02-01 15:20:14 -0500

    finish the mail function

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_openid import OpenID
 from config import basedir,ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD
+from flask.ext.mail import Mail
+
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -12,7 +14,7 @@ lm = LoginManager()
 lm.init_app(app)
 lm.login_view = 'login'
 oid = OpenID(app, os.path.join(basedir, 'tmp'))
-
+mail = Mail(app)
 
 from app import views, models
 
--- app/views.py
+++ app/views.py
@@ -7,10 +7,9 @@ from datetime import datetime
 from config import POSTS_PER_PAGE
 from config import MAX_SEARCH_RESULTS
 from forms import SearchForm
+from emails import follower_notification
 
-@lm.user_loader
-def load_user(id):
-    return User.query.get(int(id))
+
 
 @app.before_request
 def before_request():
@@ -134,7 +133,9 @@ def follow(nickname):
     db.session.add(u)
     db.session.commit()
     flash('You are now following ' + nickname + '!')
+    follower_notification(user, g.user)
     return redirect(url_for('user', nickname=nickname))
+
 
 @app.route('/unfollow/<nickname>')
 @login_required
@@ -170,6 +171,14 @@ def search_results(query):
     return render_template('search_results.html',
                            query=query,
                            results=results)
+
+#@app.route('/follow/<nickname>')
+#@login_required
+
+
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 @app.errorhandler(404)
 def not_found_error(error):
